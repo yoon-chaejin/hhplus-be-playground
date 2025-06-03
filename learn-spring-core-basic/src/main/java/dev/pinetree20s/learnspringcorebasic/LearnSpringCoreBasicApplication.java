2a22ce112138bc8c97488c1775e9b965ce9be3bd
yoon-chaejin <kevin3st@naver.com>
2025-06-03 16:58:52 +0900

    chore: learn-spring-core-basic 프로젝트 초기화

---
+++ learn-spring-core-basic/src/main/java/dev/pinetree20s/learnspringcorebasic/LearnSpringCoreBasicApplication.java
@@ -0,0 +1,13 @@
+package dev.pinetree20s.learnspringcorebasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringCoreBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpringCoreBasicApplication.class, args);
+    }
+
+}
---
+++ learn-spring-core-basic/src/test/java/dev/pinetree20s/learnspringcorebasic/LearnSpringCoreBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pinetree20s.learnspringcorebasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringCoreBasicApplicationTests {
+
+    @Test
+    void contextLoads() { // default
+    }
+
+}
