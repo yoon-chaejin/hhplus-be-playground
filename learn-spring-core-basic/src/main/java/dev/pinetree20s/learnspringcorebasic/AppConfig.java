3ddaee6b7eca37840e532742de3ba05990260e17
yoon-chaejin <kevin3st@naver.com>
2025-06-04 17:05:33 +0900

    code : 스프링 컨테이너 적용

--- learn-spring-core-basic/src/main/java/dev/pinetree20s/learnspringcorebasic/AppConfig.java
+++ learn-spring-core-basic/src/main/java/dev/pinetree20s/learnspringcorebasic/AppConfig.java
@@ -9,21 +9,26 @@ import dev.pinetree20s.learnspringcorebasic.member.MemberServiceImpl;
 import dev.pinetree20s.learnspringcorebasic.member.MemoryMemberRepository;
 import dev.pinetree20s.learnspringcorebasic.order.OrderService;
 import dev.pinetree20s.learnspringcorebasic.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
 
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
---
+++ learn-spring-core-basic/src/test/java/dev/pinetree20s/learnspringcorebasic/applicationcontext/ApplicationContextTest.java
@@ -0,0 +1,46 @@
+package dev.pinetree20s.learnspringcorebasic.applicationcontext;
+
+import dev.pinetree20s.learnspringcorebasic.AppConfig;
+import dev.pinetree20s.learnspringcorebasic.member.MemberService;
+import dev.pinetree20s.learnspringcorebasic.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+class ApplicationContextTest {
+
+    AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("모든 빈 정보 출력하기")
+    void printBeans() {
+        //given
+
+        //when
+        String[] beanDefinitionNames = context.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = context.getBean(beanDefinitionName);
+            System.out.println("name : " + beanDefinitionName + ", bean : " + bean);
+        }
+
+        //then
+    }
+
+    @Test
+    @DisplayName("애플리케이션 빈 정보 출력하기")
+    void printApplicationBeans() {
+        //given
+
+        //when
+        String[] beanDefinitionNames = context.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = context.getBeanDefinition(beanDefinitionName);
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION)
+                System.out.println("name : " + beanDefinitionName + ", bean : " + beanDefinition);
+        }
+
+        //then
+    }
+}
